88b4b37ddec6821cd4301119ad03de3499fa65f7
zzt108 <10920613+zzt108@users.noreply.github.com>
2024-10-05 22:24:47 +0200

    Bynary files dont get into the docx files

--- DocX/DocXHandler.cs
+++ DocX/DocXHandler.cs
@@ -33,7 +33,7 @@ public class DocXHandler
                     continue;
                 }
 
-                if (MimeTypeProvider.GetMimeType(extension).StartsWith("application")) // non text types should be uploaded separately
+                if (MimeTypeProvider.IsBinary(extension)) // non text types should be uploaded separately
                 {
                     continue;
                 }
@@ -71,38 +71,5 @@ public class DocXHandler
         }
     }
 
-    /*
-    public static void ConvertFilesToDocx(string folderPath, string outputDocxPath)
-    {
-        var doc = DocX.Create(outputDocxPath);
-
-        foreach (var filePath in Directory.GetFiles(folderPath))
-        {
-            // Check MIME type and upload
-            string extension = Path.GetExtension(filePath);
-            if (MimeTypeProvider.GetMimeType(extension) == "application/octet-stream") // Skip unknown types
-            {
-                continue;
-            }
-
-            // Check if the file content is not empty
-            if (new FileInfo(filePath).Length == 0)
-            {
-                continue; // Skip empty files
-            }
-
-            var mdTag = MimeTypeProvider.GetMdTag(extension);
-            string content = File.ReadAllText(filePath);
-            if (mdTag != null)
-            {
-                // Add start and end language tags to the file content
-                content = $"```{mdTag}\n{content}\n```";
-            }
-            doc.InsertParagraph(content).SpacingAfter(20);
-        }
-
-        doc.Save();
-    }
-    */
 }
 
